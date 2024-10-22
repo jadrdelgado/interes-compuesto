39f605660abe54a99232068a5b63593e1fc975d3
delgado <jadrdelgado@gmail.com>
2024-10-21 23:02:00 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components'
+
+const Conteiner = styled.div`
+  display: flex;
+  justify-content: center;
+  height: 100%;
+  align-items: center;
+`
+const Section = styled.section`
+  backgrount-color = #eee;
+  border-top: solid 2px palevioletred;
+  padding: 20px 25px;
+`
+function App() {
+  return (
+    <Conteiner>
+      lala
+    </Conteiner>
+  )
+}
+
+export default App
